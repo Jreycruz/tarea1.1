cedb857e0637a755d472363a78e35a21b7d46e45
JorgeReyes <momster478@gmail.com>
2025-02-10 13:51:54 -0600

    actu ejercicio 1

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -1,27 +1,15 @@
 function aplicarImpuestos(productos) {
-  const productosConImpuesto = [];
-
-  for (let i = 0; i < productos.length; i++) {
-    const producto = productos[i];
-
-    if (producto.precio > 50) {
-      const nuevoPrecio = +(producto.precio * 1.1).toFixed(2);
-
-      const productoActualizado = {
-        nombre: producto.nombre,
-        precio: nuevoPrecio,
-      };
-
-      productosConImpuesto.push(productoActualizado);
-    }
-  }
-
-  return productosConImpuesto;
+  return productos
+    .filter(producto => producto.precio > 50)
+    .map(producto => ({
+      ...producto,
+      precio: +(producto.precio * 1.1).toFixed(2)
+    }));
 }
 
 const productos = [
-  { nombre: "Camisa", precio: 45 },
-  { nombre: "Zapatos", precio: 80 },
+  { nombre: "Camisa", precio: 46 },
+  { nombre: "Zapatos", precio: 80 }
 ];
 
 const resultado = aplicarImpuestos(productos);
